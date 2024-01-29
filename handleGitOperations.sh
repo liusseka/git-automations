d90475472b8896ace4303857e22b5dba779c47d7
cjmatheka <cjmatheka@gmail.com>
2024-01-30 02:27:38 +0300

    mmm

--- handleGitOperations.sh
+++ handleGitOperations.sh
@@ -12,28 +12,30 @@ perform_git_operations() {
     # Commit changes with the provided message
     git commit -m "$COMMIT_MESSAGE"
 
-    # Get the name of the current directory
-    DIR_NAME=$(basename "$(pwd)")
-
     # Check if the repository exists on GitHub
-    response=$(curl -s -o /dev/null -w "%{http_code}" "https://api.github.com/repos/$GIT_USERNAME/$DIR_NAME")
+    response=$(curl -s -o /dev/null -w "%{http_code}" "https://api.github.com/repos/$GIT_USERNAME/$REPO_NAME")
 
     if [ $response -eq 404 ];
     then
-        echo "Repository $DIR_NAME does not exists on GitHub. Creating one...."
+        echo "Repository $REPO_NAME does not exists on GitHub. Creating one...."
+        # Get the name of the current directory
+        DIR_NAME=$(basename "$(pwd)")
+
+        # Set the repository name to be the same as the directory name
+        REPO_NAME="$DIR_NAME"
 
         # Create the repository using the GitHub API
-        response=$(curl -s -X POST -H "Authorization: token $GITHUB_TOKEN" -d '{"name":"'"$DIR_NAME"'"}'
+        response=$(curl -s -X POST -H "Authorization: token $GITHUB_TOKEN" -d '{"name":"'"$REPO_NAME"'"}'
         "https://api.github.com/user/repos")
 
         if [[ "$response" == *"errors"* ]];
         then
-            echo "Failed to create repository '$DIR_NAME' on GitHub. Error: $response"
+            echo "Failed to create repository '$REPO_NAME' on GitHub. Error: $response"
             exit 1
         else
-            echo "Repository '$DIR_NAME' created successfully on GitHub."
+            echo "Repository '$REPO_NAME' created successfully on GitHub."
         fi
-        git remote add origin "https://$GITHUB_TOKEN@github.com/$GIT_USERNAME/$DIR_NAME.git"
+        git remote add origin "https://$GITHUB_TOKEN@github.com/$GIT_USERNAME/$REPO_NAME.git"
     fi
 
     # Push changes to the remote repository
