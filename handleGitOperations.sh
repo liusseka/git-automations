90bc517aaf0adb10d8291def3be6fdc598134cd1
cjmatheka <cjmatheka@gmail.com>
2024-01-30 02:03:37 +0300

    mmm

--- handleGitOperations.sh
+++ handleGitOperations.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
+source handleUserInput.sh
+
 # Function to perform Git operations
 perform_git_operations() {
     # Add new files to the staging area
     git add .
 
-    read -r -p "Enter the commit message: " COMMIT_MESSAGE
-
-        # Check if the commit message is provided
-        if [ -z "$COMMIT_MESSAGE" ]; then
-            echo "Commit message cannot be empty."
-            exit 1
-        fi
+    handle_user_input
 
     # Commit changes with the provided message
     git commit -m "$COMMIT_MESSAGE"
